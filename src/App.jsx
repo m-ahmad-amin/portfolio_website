5f63fa59bee12ad9aac66bb2249d6c7872ab3f7e
m-ahmad-amin <mahmadamindw@gmail.com>
2025-07-28 08:00:12 +0500

    Style Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SkillsPage from "./pages/SkillsPage.jsx";
+import ProjectsPage from "./pages/ProjectsPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import NavBar from "./components/NavBar.jsx";
+
 export default function App() {
   return (
     <>
-      
+    <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/skills" element={<SkillsPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import { useNavigate, useLocation } from "react-router-dom";
+
+export default function NavBar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const currentPath = location.pathname;
+  return (
+    <>
+    <div className="flex justify-evenly p-3 sm:gap-[20%]">
+      <div className="px-8 sm:px-0 font-poppins flex justify-between w-[100%] sm:w-[30%]">
+        <button
+        className={currentPath === "/" ? "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-[#00f0ff] text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-[100%] after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]" : "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-white text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-0 after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]"}
+         onClick={() => {
+            navigate("/")
+        }}>Home</button>
+
+        <button
+        className={currentPath === "/skills" ? "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-[#00f0ff] text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-[100%] after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]" : "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-white text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-0 after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]"}
+         onClick={() => {
+            navigate("/skills")
+        }}>Skills</button>
+
+        <button
+        className={currentPath === "/projects" ? "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-[#00f0ff] text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-[100%] after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]" : "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-white text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-0 after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]"}
+         onClick={() => {
+            navigate("/projects")
+        }}>Projects</button>
+
+        <button
+        className={currentPath === "/contact" ? "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-[#00f0ff] text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-[100%] after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]" : "relative after:transition-[width] after:duration-[400ms] after:ease-in-out text-white text-md sm:text-lg font-extrabold after:content-[''] after:bg-[#00f0ff] after:w-0 after:absolute after:left-0 after:bottom-[-3px] after:h-[2px]"}
+         onClick={() => {
+            navigate("/contact")
+        }}>Contact</button>
+      </div>
+      <div>
+        <h1 className="text-white text-2xl font-black font-poppins hidden sm:block">Portfolio</h1>
+      </div>
+      </div>
+    </>
+  );
+}
