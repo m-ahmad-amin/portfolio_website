4b1550e47f9298664af7cc6d09a32f8b1b265113
m-ahmad-amin <mahmadamindw@gmail.com>
2026-07-11 05:57:30 +0500

    Update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ export default {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif', 'lato'],
       },
+      keyframes: {
+        "fade-up": {
+          "0%": { opacity: "0", transform: "translateY(14px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "graph-column-in": {
+          "0%": { opacity: "0", transform: "scaleY(0.4)" },
+          "100%": { opacity: "1", transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "fade-up": "fade-up 0.55s ease-out forwards",
+        "graph-column-in": "graph-column-in 0.35s ease-out forwards",
+      },
     },
     screens: {
       xxsm: "358px",
