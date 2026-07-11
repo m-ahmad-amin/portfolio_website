bc85160b4c1de008cc32e727e9f36ff3d7d36fae
m-ahmad-amin <mahmadamindw@gmail.com>
2026-07-11 05:58:10 +0500

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SkillsPage from "./pages/SkillsPage.jsx";
 import ProjectsPage from "./pages/ProjectsPage.jsx";
+import ProjectDetailPage from "./pages/ProjectDetailPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import NavBar from "./components/NavBar.jsx";
 
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/skills" element={<SkillsPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </>
