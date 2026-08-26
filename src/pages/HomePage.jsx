8f71e1b6e106b358ff190aae5c66f6727c9253d8
m-ahmad-amin <mahmadamindw@gmail.com>
2026-08-26 20:28:58 +0500

    Add kaggle

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { SiCodeforces, SiFacebook, SiGithub, SiInstagram, SiLeetcode, SiLinkedin } from "react-icons/si";
+import { SiCodeforces, SiFacebook, SiGithub, SiInstagram, SiKaggle, SiLeetcode, SiLinkedin } from "react-icons/si";
 import Lottie from "lottie-react";
 import homePageAnimation from "./../assets/animations/homePageAnimation.json";
 import CodingGraph from "../components/CodingGraph";
@@ -30,8 +30,8 @@ export default function HomePage() {
                             <SiCodeforces className="transition-all duration-300 ease-in-out hover:cursor-pointer hover:scale-110" />
                         </a>
 
-                        <a>
-                            <SiInstagram className="transition-all duration-300 ease-in-out hover:cursor-pointer hover:scale-110" />
+                        <a href="https://www.kaggle.com/ahmadamindw" target="_blank">
+                            <SiKaggle className="transition-all duration-300 ease-in-out hover:cursor-pointer hover:scale-110" />
                         </a>
                     </div>
                 </div>
